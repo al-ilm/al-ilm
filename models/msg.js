2cf0f5219c26effec980d4fc8e5fff8d2b4caee3
adammmr <amkfd1@gmail.com>
2023-12-29 17:17:56 +0100

    first commit

---
+++ models/msg.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  s_name: {
+    type: String,
+    required: true,
+  },
+  s_email: {
+    type: String,
+    required: true,
+  },
+  s_message: {
+    type: String,
+    required: true,
+  },
+  s_phone: {
+    type: String,
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Message = mongoose.model('Msg', messageSchema);
+
+module.exports = Message;
